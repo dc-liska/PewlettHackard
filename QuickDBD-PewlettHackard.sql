be570c25a1f623a17ed5e5822b491016315b5677
David Liska <liskadavid@gmail.com>
2019-07-30 14:03:58 -0500

    Completed ERD ver.1

--- QuickDBD-PewlettHackard.sql
+++ QuickDBD-PewlettHackard.sql
@@ -13,20 +13,20 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INTEGER   NOT NULL,
-    "dept_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     "from_date" DATE   NOT NULL,
     "to_date" DATE   NOT NULL
 );
 
 CREATE TABLE "dept_manager" (
     "emp_no" INTEGER   NOT NULL,
-    "dept_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     "from_date" DATE   NOT NULL,
     "to_date" DATE   NOT NULL
 );
 
 CREATE TABLE "employees" (
-    "emp_no" INTEGER   NOT NULL,
+    "emp_no" SERIAL   NOT NULL,
     "birth_date" DATE   NOT NULL,
     "first_name" VARCHAR(30)   NOT NULL,
     "last_name" VARCHAR(30)   NOT NULL,
@@ -35,6 +35,20 @@ CREATE TABLE "employees" (
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "emp_no" INTEGER   NOT NULL,
+    "title" VARCHAR(25)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
 );
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
@@ -48,4 +62,10 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
